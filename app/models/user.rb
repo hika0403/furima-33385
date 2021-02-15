ea53b80434039dfa172b40db83d52443628cbe85
hika0403 <egirlshikaru_0221@icloud.com>
2021-02-15 21:33:43 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  validates :nickname, :birth_day, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-      validates :last_name_kanji
-      validates :first_name_kanji
-    with_options format: { with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）を使用してください' } do
-      validates :last_name_katakana
-      validates :first_name_katakana
-    end
+    validates :last_name_kanji
+    validates :first_name_kanji
   end
-  validates :birth_day, presence: true
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）を使用してください' } do
+    validates :last_name_katakana
+    validates :first_name_katakana
+  end
 end
