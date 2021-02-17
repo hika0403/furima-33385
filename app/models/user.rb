8e8376d222c9bb729c0d1b004bdd9cc353d32d8e
hika0403 <egirlshikaru_0221@icloud.com>
2021-02-17 11:38:59 +0900

    with_optionsでの入れ子修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-  validates :nickname, :birth_day, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :last_name_kanji
-    validates :first_name_kanji
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）を使用してください' } do
-    validates :last_name_katakana
-    validates :first_name_katakana
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
+    validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze }
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+      validates :last_name_kanji
+      validates :first_name_kanji
+    end
+
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: '全角（カタカナ）を使用してください' } do
+      validates :last_name_katakana
+      validates :first_name_katakana
+    end
   end
 end
