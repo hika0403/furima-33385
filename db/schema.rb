8d15c45b3b7f5e66e8338d50ab9975358958fd5d
hika0403 <egirlshikaru_0221@icloud.com>
2021-02-27 15:15:57 +0900

    user_idカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_104107) do
+ActiveRecord::Schema.define(version: 2021_02_27_044705) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_104107) do
     t.integer "area_id", null: false
     t.integer "shopping_day_id", null: false
     t.integer "price", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
