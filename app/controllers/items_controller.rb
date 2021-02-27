ea577ac9fcb4e64e53b619de2b9c8f9069e0b634
hika0403 <egirlshikaru_0221@icloud.com>
2021-02-27 15:16:30 +0900

    出品後の遷移実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,10 +9,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :text, :category_id, :situation_id, :charge_id, :area_id, :shopping_day_id, :price).merge(user_id: current_user.id)
   end
 end
