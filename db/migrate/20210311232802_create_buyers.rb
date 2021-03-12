1b28c3884df30aaae309163a54c76e2f60a1dca8
hika0403 <egirlshikaru_0221@icloud.com>
2021-03-12 09:39:42 +0900

    マイグレーションの実装

--- db/migrate/20210311232802_create_buyers.rb
+++ db/migrate/20210311232802_create_buyers.rb
@@ -1,7 +1,13 @@
 class CreateBuyers < ActiveRecord::Migration[6.0]
   def change
     create_table :buyers do |t|
-
+      t.string :postal_code,  default:"", null: false
+      t.integer :prefecture,              null: false
+      t.string :town,         default:"", null: false
+      t.string :address,      default:"", null: false
+      t.string :building,     default:""
+      t.string :phone_number, default:"", null: false
+      t.references :order,                null: false, foreign_key: true
       t.timestamps
     end
   end
