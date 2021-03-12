7f4892284b2f7b75abaa856aadec50d16db99316
hika0403 <egirlshikaru_0221@icloud.com>
2021-03-12 09:40:39 +0900

    バリデーションのスペル修正

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -3,11 +3,11 @@ class OrderBuyer
   attr_accessor :postal_code, :prefecture, :town, :address, :building, :phone_number
 
   with_options presence: true do
-    valivates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    valivates :prefecture, numericality: { other_than: 0 }
-    valivates :town
-    valivates :address
-    valivates :phone_number, format: { with: /\A[0-9]{11}\z/ }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :prefecture, numericality: { other_than: 0 }
+    validates :town
+    validates :address
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/ }
   end
 
   def save
