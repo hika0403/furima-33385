198fff27d1bb11dc8ae4ccd03607d3d25e2bffa7
hika0403 <egirlshikaru_0221@icloud.com>
2021-03-12 19:30:39 +0900

    コントローラーの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,22 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @order_buyer = OrderBuyer.new
   end
 
   def create
+    @order_buyer = OrderBuyer.new(buyer_params)
+    if @order_buyer.valid?
+      @order_buyer.save
+      redirect_to root_path
+    else 
+      @item = Item.find(params[:item_id])
+      render action: :index
+    end
+  end
+
+  private
+  def buyer_params
+    params.require(:order_buyer).permit(:postal_code, :prefecture, :town, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
