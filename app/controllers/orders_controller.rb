482a2e62a9ddcbc7431ab46753d613f1fd0ba5fc
hika0403 <egirlshikaru_0221@icloud.com>
2021-03-14 08:26:15 +0900

    条件分岐の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,6 +36,6 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
