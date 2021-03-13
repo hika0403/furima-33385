42f923f172b9c0b8bcd47b32c6b73d7decffc066
hika0403 <egirlshikaru_0221@icloud.com>
2021-03-13 14:03:07 +0900

    コードのまとめとログイン者によっての遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: :index
 
   def index
-    @item = Item.find(params[:item_id])
     @order_buyer = OrderBuyer.new
   end
 
@@ -12,13 +13,22 @@ class OrdersController < ApplicationController
       @order_buyer.save
       redirect_to root_path
     else 
-      @item = Item.find(params[:item_id])
       render action: :index
     end
   end
 
   private
+
   def buyer_params
     params.require(:order_buyer).permit(:postal_code, :prefecture, :town, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
 end
