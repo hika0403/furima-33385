7952190e97f4801e679885e69faf3dd960f8dc4d
hika0403 <egirlshikaru_0221@icloud.com>
2021-03-14 09:10:00 +0900

    テストコードの修正

--- spec/factories/order_buyer.rb
+++ spec/factories/order_buyer.rb
@@ -1,11 +1,13 @@
 FactoryBot.define do
   factory :order_buyer do
     postal_code  { '123-1234' }
-    prefecture   { '2' }
+    prefecture   { 2 }
     town         { '横浜市南区' }
-    address      { '123' }
+    address      { 123 }
     building     { '横浜ハイツ' }
     phone_number { '09012345678' }
     token        { 'sk_123_abc_456' }
+    item_id      { 2 }
+    user_id      { 4 }
   end
 end
