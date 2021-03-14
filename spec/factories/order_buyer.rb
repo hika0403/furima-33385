7e7101ff5da85be763353e8f551c7dfd5cc53bcf
hika0403 <egirlshikaru_0221@icloud.com>
2021-03-14 11:37:27 +0900

    外部キーのテストコード修正

--- spec/factories/order_buyer.rb
+++ spec/factories/order_buyer.rb
@@ -7,7 +7,5 @@ FactoryBot.define do
     building     { '横浜ハイツ' }
     phone_number { '09012345678' }
     token        { 'sk_123_abc_456' }
-    item_id      { 2 }
-    user_id      { 4 }
   end
 end
--- spec/models/order_buyer_spec.rb
+++ spec/models/order_buyer_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe OrderBuyer, type: :model do
   describe '購入情報の保存' do
     before do
-      @order_buyer = FactoryBot.build(:order_buyer)
+      @user = FactoryBot.create(:user)
+      @item = FactoryBot.build(:item)
+      image = fixture_file_upload('public/images/test_image.png')
+      @item.save
+      @order_buyer = FactoryBot.build(:order_buyer, user_id: @user.id, item_id: @item.id)
     end
 
     context '購入情報が登録できるとき' do
